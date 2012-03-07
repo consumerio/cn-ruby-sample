4a3234a22c2bc213c322bcae554f09be20c8c1c5
Daniel Greenfeld <pydanny@gmail.com>
2012-03-06 19:20:54 -0800

    Adding back

---
+++ README.md
@@ -0,0 +1,5 @@
+Very Simple Ruby Samples by a Ruby Noob
+=======================================
+
+Just some easy examples built the way you do it when you figure Ruby from Stack Overflow answers.
+
--- lists.rb
+++ lists.rb
@@ -2,6 +2,6 @@
 
 require 'rest_client'
 
-response = RestClient.get('https://consumernotebook.com/api/v1/lists/?format=json&apikey={apikey}')
+response = RestClient.get('https://consumernotebook.com/api/v1/lists/?format=json&apikey={apikey}');
 
 puts response.code
